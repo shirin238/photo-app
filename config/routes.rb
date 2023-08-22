ab71fc8f6d3613a3dc8d3f71f6e4ea9a6b8c9f1c
shi <shirin23.dev@gmail.com>
2023-08-22 15:55:45 +0200

    remove devise and import bootstrap as per failing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+
+  root "application#hello"
+
   get 'static_pages/home'
   get 'static_pages/help'
-  devise_for :users
-  #get 'welcome/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "welcome#index"
+    
 end
