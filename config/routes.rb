225f5fb6b43686cd4546e8c9e7ce847f5f4f9d31
shi <shirin23.dev@gmail.com>
2023-08-24 16:15:54 +0200

    add User controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/help', to: "static_pages#help", as: "help"
   get '/about', to: "static_pages#about", as: "about"
   get '/contact', to: "static_pages#contact", as: "contact"
+  get '/signup', to: "users#new", as: "signup"
   
 
     
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get signup_path
     assert_response :success
+    assert_select "title", "Sign Up | Ruby on Rails Tutorial Sample App"
   end
 end
