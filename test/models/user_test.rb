fa742d2ba6afc95350f7a7615d3f46d4dff0c75d
shi <shirin23.dev@gmail.com>
2023-08-24 17:47:40 +0200

    add password validations to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,9 @@ class UserTest < ActiveSupport::TestCase
 
 
   def setup
-    @user = User.new(name: "example_user", email: "example_email@gmail.com")
+    @user = User.new(name: "example_user", email: "example_email@gmail.com",
+                    password: "foobar", password_confirmation: "foobar")
+
   end
 
 
@@ -25,4 +27,46 @@ class UserTest < ActiveSupport::TestCase
   end
 
 
+  test "email should not be too long"  do
+    @user.email = "a"*244
+    assert_not @user.valid?
+  end
+
+
+  test "name should not be too long"  do
+    @user.name = "a"*60
+    assert_not @user.valid?
+  end
+
+
+  test "email addresses should be unique"  do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblanc)"  do
+    @user.password = @user.password_confirmation = " "*6
+    assert_not @user.valid?
+  end
+
+
+  test "password should have a minimum lenght"  do
+    @user.password = @user.password_confirmation = "a"*5
+    assert_not @user.valid?
+  end
+
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
